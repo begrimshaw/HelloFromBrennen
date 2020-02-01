9f770d22f10cb39189613e2329101777a0c47494
Megan Allred <meg21allred@gmail.com>
2020-01-31 19:22:17 -0500

    add hello message to brennen's file

--- src/grimshaw/HelloFromBrennen.java
+++ src/grimshaw/HelloFromBrennen.java
@@ -3,6 +3,7 @@ package grimshaw;
 public class HelloFromBrennen {
     public static void main(String[] args) {
         System.out.println("Hello my name is Brennen");
+        System.out.println(("Hello, Brennen, my name is Megan"));
     }
 }
 
